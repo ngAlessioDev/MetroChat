acc0d4c1fa9b02b7177b2936ab494534d938168b
System Administrator <root@MacBook-Pro-di-Alessio.local>
2024-01-12 18:54:32 +0100

    add menu3

---
+++ src/app/menu3/menu3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu3',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu3.component.html',
+  styleUrl: './menu3.component.css'
+})
+export class Menu3Component {
+
+}
