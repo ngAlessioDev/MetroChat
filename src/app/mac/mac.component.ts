81792f917b7bc8314381d5e004756161294334e6
Alessio <alessioroggero@gmail.com>
2024-02-03 15:10:50 +0100

    push by mac PC

---
+++ src/app/mac/mac.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mac',
+  standalone: true,
+  imports: [],
+  templateUrl: './mac.component.html',
+  styleUrl: './mac.component.css'
+})
+export class MacComponent {
+
+}
