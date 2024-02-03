d0e7ffe46e6c9acb420d6c99bae7aa2cb1252c78
Alessio <alessioroggero@gmail.com>
2024-02-03 15:22:27 +0100

    aggiunti 2 componenti

---
+++ src/app/linux/linux.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-linux',
+  standalone: true,
+  imports: [],
+  templateUrl: './linux.component.html',
+  styleUrl: './linux.component.css'
+})
+export class LinuxComponent {
+
+}
---
+++ src/app/ubuntu/ubuntu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ubuntu',
+  standalone: true,
+  imports: [],
+  templateUrl: './ubuntu.component.html',
+  styleUrl: './ubuntu.component.css'
+})
+export class UbuntuComponent {
+
+}
