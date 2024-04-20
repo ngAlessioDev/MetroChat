001651f993a5eb6a5a89e56829729b298ecc46b1
Alessio <alessioroggero@gmail.com>
2024-04-20 20:22:43 +0200

    iniziato menu' con interpolazione

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    
     
   ],
   imports: [
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+link_menu_1: string;
+link_menu_2: string;
+link_menu_3: string;
+constructor(){
+  this.link_menu_1 ="Treni";
+  this.link_menu_2 ="Preferiti";
+  this.link_menu_3 ="Login";
 
-}
+}}
